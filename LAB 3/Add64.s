33f98b11a36ce0f7358f7b0145232aa66be5df3d
abhay-iy97 <abhay.iyer6@gmail.com>
2017-02-21 01:33:49 +0530

    Create Add64.s

---
+++ LAB 3/Add64.s
@@ -0,0 +1,34 @@
+  AREA RESET,DATA,READONLY
+	EXPORT __Vectors
+
+__Vectors
+	DCD 0x10001000
+	DCD Reset_Handler
+	ALIGN
+	
+	AREA mycode,CODE,READONLY
+	ENTRY
+	EXPORT Reset_Handler
+
+Reset_Handler
+	LDR R0,=num1
+	LDR R1,=num2
+	LDR R6,=sum
+	LDR R3,[R0],#4
+	LDR R4,[R1],#4
+	
+	ADDS R5,R3,R4
+	STR R5,[R6],#4
+	
+	LDR R3,[R0]
+	LDR R4,[R1]
+	
+	ADCS R5,R3,R4
+	STR R5,[R6]
+	
+STOP B STOP
+num1 DCD 0x00000001,0x0,0x0,0x0
+num2 DCD 0x00000001,0x0,0x0,0x0
+	AREA mydata,DATA,READWRITE
+sum DCD 0,0
+	END
